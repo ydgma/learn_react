fbe33f432532a8bfcab5de9b8cb9c4c0493074a9
Yasiru Dahanayake <yasirudahanayake@gmail.com>
2022-02-26 11:59:15 +0000

    26/02/2022 adding conditional logic to rendering

--- src/components/Expenses/Expences.js
+++ src/components/Expenses/Expences.js
@@ -1,8 +1,8 @@
 import Card from "../UI/Card";
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -11,22 +11,9 @@ const Expenses = (props) => {
     setFilteredYear(year);
   };
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  const filteredExpense = props.items.filter((expense) => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-
-  if (filteredExpense.length > 0) {
-    expensesContent =  filteredExpense.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
 
   return (
     <div>
@@ -35,7 +22,7 @@ const Expenses = (props) => {
           selectedYear={filteredYear}
           onYearChange={filterChangeHandler}
         />
-        {expensesContent} 
+        <ExpensesList items={filteredExpenses}/> 
       </Card>
     </div>
   );
